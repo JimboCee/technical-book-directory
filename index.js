a0d4e2fe43771a7e5983b035d3cd42e8293203c4
JimboCee <49816802+JimboCee@users.noreply.github.com>
2022-04-06 12:28:19 +0100

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const api = require('./src/api');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json());
 app.use('/api/v1', api);
